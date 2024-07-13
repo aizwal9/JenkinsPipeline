53fb7a2a89194118dd26782c237f4ccb9c0c9b4d
aizwal <jaiswalaman97@gmail.com>
2024-07-14 00:49:35 +0530

    first commit

---
+++ src/main/java/com/microservices/JenkinsPipelineApplication.java
@@ -0,0 +1,18 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsPipelineApplication {
+
+    @GetMapping
+    public String message(){return "welcome to jenkins projects";}
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPipelineApplication.class,args);
+    }
+}
